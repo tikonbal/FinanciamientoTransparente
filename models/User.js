7b75e22a7cd8f20067ed356cd1faac20bf982719
Christian Paniagua <elchristianhack@gmail.com>
2015-07-12 10:58:52 -0600

    Tipos de usuarios

--- models/User.js
+++ models/User.js
@@ -14,6 +14,10 @@ User.add({
 	password: { type: Types.Password, initial: true, required: true }
 }, 'Permissions', {
 	isAdmin: { type: Boolean, label: 'Usuario de Administrador', index: true }
+	isTse: { type: Boolean, label: 'Usuario de Tse', index: true }
+	isOp: { type: Boolean, label: 'Usuario de Op', index: true }
+	isMediocom: { type: Boolean, label: 'Usuario de Medio de Comunicacion', index: true }
+	isAsiosaciones: { type: Boolean, label: 'Usuario de Asiosaciones Civiles', index: true }
 });
 
 // Provide access to Keystone
